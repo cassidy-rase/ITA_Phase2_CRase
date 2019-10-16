5306793de4d06ca128ce7a16993127987baf8a4f
cassidy-rase <crase@redventures.com>
2019-10-16 17:14:52 -0400

    updating MySQL

--- week19/practice2.sql
+++ week19/practice2.sql
@@ -1,2 +1,11 @@
 -- 15) When was the first order?
 SELECT min(OrderDate) as FirstOrder FROM Orders;
+
+-- 16) Countries where there are customers
+SELECT DISTINCT Country FROM Customers ORDER BY Country;
+
+-- 17) Contact titles for customers
+SELECT ContactTitle, count(*) as TotalContactTitle FROM Customers GROUP BY ContactTitle ORDER BY count(*) desc;
+
+-- 18) Products with associated supplier names
+SELECT ProductID, ProductName, CompanyName as Supplier FROM Products Pr JOIN Suppliers Su on Pr.SupplierID = Su.SupplierID;
